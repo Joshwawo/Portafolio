2007369b1aa081bbb6660a1a2c4db6cefff00cd4
Jorge Alberto  Morales Gutierrez <masacota_masacota@hotmail.com>
2021-06-21 18:25:29 -0700

    Fin Seccion 7

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+import { ProductoDescripcion } from '../../interfaces/producto-descripcion.interface';
+import { Producto } from '../../interfaces/producto.interface';
 
 @Component({
   selector: 'app-item',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
+  producto: ProductoDescripcion;
+  id: string;
+  
 
-  constructor() { }
+  constructor(private route:ActivatedRoute,
+    public productoService: ProductosService) { }
 
   ngOnInit(): void {
+    this.route.params
+    .subscribe(parametros=>{
+      console.log(parametros['id']);
+
+      this.productoService.getProducto(parametros['id'])
+      .subscribe((producto: ProductoDescripcion) =>{
+        this.producto=producto;
+        this.id=parametros['id'];
+        console.log(producto)
+      })
+
+    })
   }
 
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { infopagina } from '../../interfaces/info-pagina.interface';
 import { InfopaginaService } from '../../services/infopagina.service';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,9 +10,19 @@ import { InfopaginaService } from '../../services/infopagina.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(public _servicio: InfopaginaService) { }
+  constructor(public _servicio: InfopaginaService,
+              private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  buscarProducto(termino:string){
+      if(termino.length<1){
+        return;
+      }
+    console.log(termino)
+    this.router.navigate(['/search',termino]);
+
   }
 
 }
